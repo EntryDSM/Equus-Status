b16e64a12499946fd9a87837e8b4a0ef49a9717c
seongwon <seongwon1290@dsm.hs.kr>
2023-11-09 23:26:55 +0900

    setting: Create Project

---
+++ src/main/kotlin/com/example/equusstatus/EquusStatusApplication.kt
@@ -0,0 +1,11 @@
+package com.example.equusstatus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EquusStatusApplication
+
+fun main(args: Array<String>) {
+    runApplication<EquusStatusApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/equusstatus/EquusStatusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.equusstatus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EquusStatusApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
