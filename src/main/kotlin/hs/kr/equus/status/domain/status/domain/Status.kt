23ad18bbbff371bfdb7e3351e98c1dae27c51def
seongwon <seongwon1290@dsm.hs.kr>
2024-01-02 10:53:47 +0900

    file: Status

---
+++ src/main/kotlin/hs/kr/equus/status/domain/status/domain/Status.kt
@@ -0,0 +1,22 @@
+package hs.kr.equus.status.domain.status.domain
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Status(
+    var isPrintsArrived: Boolean = false,
+    var isSubmitted: Boolean = false,
+    @Column(unique = true)
+    var examCode: String? = null,
+    var isFirstRoundPass: Boolean = false,
+    var isSecondRoundPass: Boolean = false,
+    val receiptCode: Long,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
+}
---
+++ src/main/kotlin/hs/kr/equus/status/domain/status/domain/repository/StatusRepository.kt
@@ -0,0 +1,8 @@
+package hs.kr.equus.status.domain.status.domain.repository
+
+import hs.kr.equus.status.domain.status.domain.Status
+import org.springframework.data.repository.CrudRepository
+
+interface StatusRepository : CrudRepository<Status, Long> {
+    fun findByReceiptCode(receiptCode: Long): Status?
+}
