6efe4762a0f94a97c3dffa9edc25d938067bfa95
seongwon <seongwon1290@dsm.hs.kr>
2024-04-15 14:04:52 +0900

    refactor: ktlint

--- src/test/kotlin/hs/kr/equus/status/domain/status/service/CancelApplicationSubmitServiceTest.kt
+++ src/test/kotlin/hs/kr/equus/status/domain/status/service/CancelApplicationSubmitServiceTest.kt
@@ -3,11 +3,11 @@ package hs.kr.equus.status.domain.status.service
 import hs.kr.equus.status.domain.status.domain.Status
 import hs.kr.equus.status.domain.status.domain.repository.StatusRepository
 import hs.kr.equus.status.domain.status.exception.StatusNotFoundException
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.given
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.assertThrows
+import org.mockito.kotlin.given
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 
@@ -27,17 +27,17 @@ class CancelApplicationSubmitServiceTest {
 
     @Test
     fun 원서접수_취소_성공() {
-        //given
+        // given
         val status = Status(
             isSubmitted = true,
             receiptCode = 1L
         )
         given(statusRepository.findByReceiptCode(receiptCode))
             .willReturn(status)
-        //when
+        // when
         cancelApplicationSubmitService.execute(receiptCode)
 
-        //then
+        // then
         assertEquals(status.isSubmitted, false)
     }
 
@@ -45,7 +45,7 @@ class CancelApplicationSubmitServiceTest {
     fun 상태_엔티티_없을시_실패() {
         given(statusRepository.findByReceiptCode(receiptCode))
             .willReturn(null)
-        //when
+        // when
         assertThrows<StatusNotFoundException> { cancelApplicationSubmitService.execute(receiptCode) }
     }
 }
