c7b2c487a1ac230194cba41d775135ffe250c6c4
HyunSu1768 <azxcv1768@gmail.com>
2024-10-20 22:47:45 +0900

    refactor :: ktlint

--- src/main/kotlin/hs/kr/equus/status/domain/status/presentation/InternalStatusController.kt
+++ src/main/kotlin/hs/kr/equus/status/domain/status/presentation/InternalStatusController.kt
@@ -4,7 +4,12 @@ import hs.kr.equus.status.domain.status.presentation.dto.response.InternalStatus
 import hs.kr.equus.status.domain.status.service.GetAllStatusService
 import hs.kr.equus.status.domain.status.service.GetStatusByReceiptCodeService
 import hs.kr.equus.status.domain.status.service.UpdateExamCodeService
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/internal/status")
